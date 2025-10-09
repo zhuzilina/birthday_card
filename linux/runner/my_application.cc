a0265b2920f4f5ac2ddec8c5ff602abf0ea1e9c0
zhuzilina <j.zilin@qq.com>
2025-10-09 20:37:07 +0800

    对桌面端平台添加了个性化适配

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -46,11 +46,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "birthday_card");
+    gtk_header_bar_set_title(header_bar, "小礼物");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "birthday_card");
+    gtk_window_set_title(window, "小礼物");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
